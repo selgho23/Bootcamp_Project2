16c426d292e26c4845e367834c5f93a76b095990
Khushboo Agarwal <agarwalkhushboo.agarwal@gmail.com>
2019-09-24 09:21:42 -0500

    merged main page

---
+++ Rough_version/app.py
@@ -0,0 +1,42 @@
+import os
+import pandas as pd
+import numpy as np
+import requests
+import bs4 
+import time
+from bs4 import BeautifulSoup as bs
+from splinter import Browser
+from pprint import pprint
+import config
+from config import api_key
+import json
+from flask import Flask, jsonify, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    """Return the homepage."""
+    return render_template("index.html")
+
+@app.route("/trivia")
+def jokes():
+    joke_response = requests.get(f"https://api.spoonacular.com/food/trivia/random", params={"apiKey" : api_key})
+    joke_json = joke_response.json()
+    return jsonify(joke_json)
+
+@app.route('/wine_pairing-page')
+def wine_pairing():
+    return render_template("wine_pairing.html")
+
+
+if __name__ == "__main__":
+    app.run(debug="true")
+
+
+
+
+
+
+
